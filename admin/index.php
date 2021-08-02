0793dd4b344e632ccf2e16bf1c9134cc92758388
white developer <52850655+mrwhitedeveloper@users.noreply.github.com>
2021-08-02 19:25:56 +0530

    first commit

---
+++ admin/index.php
@@ -0,0 +1,16 @@
+<?php include("header.php"); ?>
+
+<div class="bg-white" style="padding:10px;">
+<h4 class="text-muted my-3">
+
+</h4>
+<div class="container">
+
+
+
+</div>
+
+
+
+
+<?php include("footer.php"); ?>
